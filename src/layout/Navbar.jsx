f0c4ddf4a844e11656e912ccc3cac040d6fa8326
Karuppaiya murugan <karuppaiya.fullstackdev@gmail.com>
2026-04-12 12:12:13 +0530

    Update Navbar.jsx

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -3,8 +3,8 @@ import { Menu, X } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const navLinks = [
-  { href: "#about", label: "About" },
   { href: "#skills", label: "Skills"},
+  { href: "#about", label: "About" },
   { href: "#projects", label: "Projects" },
   // { href: "#experience", label: "Experience" },
   // { href: "#testimonials", label: "Testimonials" },
